a07dfbe860bcf604a36f95ee1b58f5600ba9f42f
Byran Zaugg <byran.zaugg@liferay.com>
2013-04-24 16:57:50 -0700

    LPS-34199 Setup script. svn:externals workaround. Custom release for ee builds.

---
+++ setup-git-svn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Setup git-svn
+git svn init http://svn.ckeditor.com/CKEditor --prefix=svn/ --stdlayout --branches=branches/versions --ignore-paths="^(CKEditor/trunk|(CKEditor/branches/versions|CKEditor/tags)/(3[^\\\.]|3\\\.[^6]))";
+#--ignore-paths allows us to download only the branches/tags we care about. ie. 3.6.x
+
+echo 'This may take a while (30+ min), depending on the staleness of your repository.';
+git svn fetch --log-window-size=10000 --revision 7691:HEAD;
+
+update-externals.sh
---
+++ update-externals.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Get current SVN Revision
+REV=$(git log -1 --pretty=%B svn/3.6.x | grep 'git-svn-id:' | sed 's/^.*@//' | sed s:\ [A-z0-9\-]*$::g);
+
+if [ -z "$REV" ]; then
+	REV="7691";
+fi;
+
+echo 'Using SVN revision: '$REV;
+
+#Download svn:externals
+curl -o _dev/langtool/langtool/langtool.exe http://svn.fckeditor.net/CKLangTool/trunk/bin/langtool.exe?p=$REV;
+curl -o _dev/langtool/langtool/langtool.jar http://svn.fckeditor.net/CKLangTool/trunk/bin/langtool.jar?p=$REV;
+curl -o _dev/releaser/ckreleaser/ckreleaser.exe http://svn.fckeditor.net/CKReleaser/trunk/bin/ckreleaser.exe?p=$REV;
+curl -o _dev/releaser/ckreleaser/ckreleaser.jar http://svn.fckeditor.net/CKReleaser/trunk/bin/ckreleaser.jar?p=$REV;
