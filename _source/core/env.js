e319000ba8f858b99f9302cc5102c67666f067a7
Dustin Ryerson <dustin.ryerson@liferay.com>
2014-06-30 14:58:29 -0700

    IE11 is still IE. Don't fool us. (https://github.com/liferay/ckeditor-ee/commit/a70b073416e38f54dd7184b7c31603bd1573808f)

--- _source/core/env.js
+++ _source/core/env.js
@@ -28,7 +28,7 @@ if ( !CKEDITOR.env )
 			 * if ( CKEDITOR.env.ie )
 			 *     alert( "I'm on IE!" );
 			 */
-			ie		: /*@cc_on!@*/false,
+			ie		: agent.indexOf( 'trident/' ) > -1,
 
 			/**
 			 * Indicates that CKEditor is running on Opera.
@@ -133,7 +133,7 @@ if ( !CKEDITOR.env )
 		 * if ( CKEDITOR.env.gecko )
 		 *     alert( "I'm riding a gecko!" );
 		 */
-		env.gecko = ( navigator.product == 'Gecko' && !env.webkit && !env.opera );
+		env.gecko = ( navigator.product == 'Gecko' && !env.webkit && !env.opera && !env.ie );
 
 		var version = 0;
 
