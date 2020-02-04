3daf6321723156455b30c44f867e55d51a7c9058
Marko Čikoš <marko.cikos@liferay.com>
2020-02-04 16:43:40 +0100

    patch: LPS-107869 Update CKEditor to 4.13.1

--- ckeditor/plugins/uploadwidget/lang/et.js
+++ ckeditor/plugins/uploadwidget/lang/et.js
@@ -2,4 +2,4 @@
  Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
-CKEDITOR.plugins.setLang("uploadwidget","et",{abort:"Upload aborted by the user.",doneOne:"Fail on üles laaditud.",doneMany:"Successfully uploaded %1 files.",uploadOne:"Uploading file ({percentage}%)...",uploadMany:"Uploading files, {current} of {max} done ({percentage}%)..."});
+CKEDITOR.plugins.setLang("uploadwidget","et",{abort:"Kasutaja katkestas üleslaadimise.",doneOne:"Fail on üles laaditud.",doneMany:"%1 faili laaditi edukalt üles.",uploadOne:"Faili üleslaadimine ({percentage}%)...",uploadMany:"Failide üleslaadimine, {current} fail {max}-st üles laaditud ({percentage}%)..."});
