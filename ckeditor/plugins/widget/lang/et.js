19ed38a2c52f3683779ca6f97034f5e561f235d8
Marko Čikoš <marko.cikos@liferay.com>
2020-02-04 16:43:40 +0100

    LPS-107869 Update CKEditor to 4.13.1

--- ckeditor/plugins/widget/lang/et.js
+++ ckeditor/plugins/widget/lang/et.js
@@ -2,4 +2,4 @@
  Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
-CKEDITOR.plugins.setLang("widget","et",{move:"Click and drag to move",label:"%1 vidin"});
+CKEDITOR.plugins.setLang("widget","et",{move:"Liigutamiseks klõpsa ja lohista",label:"%1 vidin"});
