7c76c76ce6f858e7c6b188f6f1b7418f5c9e7f0a
Greg Hurrell <greg.hurrell@liferay.com>
2020-04-13 11:34:28 +0200

    style: apply formatting changes

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 function main() {
-    checkVersion();
+	checkVersion();
 }
 
 /**
@@ -14,62 +14,62 @@ function main() {
  *
  */
 function checkVersion() {
-    const version = process.env.npm_package_version;
+	const version = process.env.npm_package_version;
 
-    if (!version) {
-        die("Expected non-empty npm_package_version environment variable");
-    }
+	if (!version) {
+		die('Expected non-empty npm_package_version environment variable');
+	}
 
-    let ckeditorVersion;
+	let ckeditorVersion;
 
-    try {
-        ({ version: ckeditorVersion } = require("./ckeditor-dev/package.json"));
-    } catch (_error) {
-        die("Unable to read version from ckeditor-dev/package.json");
-    }
+	try {
+		({version: ckeditorVersion} = require('./ckeditor-dev/package.json'));
+	} catch (_error) {
+		die('Unable to read version from ckeditor-dev/package.json');
+	}
 
-    if (!ckeditorVersion || !/^\d+\.\d+\.\d+$/.test(ckeditorVersion)) {
-        die(`Unexpected CKEditor version: ${ckeditorVersion}`);
-    }
+	if (!ckeditorVersion || !/^\d+\.\d+\.\d+$/.test(ckeditorVersion)) {
+		die(`Unexpected CKEditor version: ${ckeditorVersion}`);
+	}
 
-    const regexp = new RegExp(`^${ckeditorVersion}-liferay\\.\\d+$`);
+	const regexp = new RegExp(`^${ckeditorVersion}-liferay\\.\\d+$`);
 
-    if (!regexp.test(version)) {
-        die(
-            `Proposed version ${JSON.stringify(
-                version
-            )} does not match expected format (${regexp})`
-        );
-    }
+	if (!regexp.test(version)) {
+		die(
+			`Proposed version ${JSON.stringify(
+				version
+			)} does not match expected format (${regexp})`
+		);
+	}
 
-    let builtVersion;
+	let builtVersion;
 
-    try {
-        const source = fs.readFileSync(
-            path.join(__dirname, "ckeditor", "ckeditor.js"),
-            "utf8"
-        );
+	try {
+		const source = fs.readFileSync(
+			path.join(__dirname, 'ckeditor', 'ckeditor.js'),
+			'utf8'
+		);
 
-        [, builtVersion] = source.match(/\bversion\s*:\s*"(\d+\.\d+\.\d+)/);
-    } catch (_error) {
-        die("Failed to read built version from ckeditor/ckeditor.js");
-    }
+		[, builtVersion] = source.match(/\bversion\s*:\s*"(\d+\.\d+\.\d+)/);
+	} catch (_error) {
+		die('Failed to read built version from ckeditor/ckeditor.js');
+	}
 
-    if (builtVersion !== ckeditorVersion) {
-        die(
-            `Built CKEditor version (${builtVersion}) does not match submodule version (${ckeditorVersion})`
-        );
-    }
+	if (builtVersion !== ckeditorVersion) {
+		die(
+			`Built CKEditor version (${builtVersion}) does not match submodule version (${ckeditorVersion})`
+		);
+	}
 }
 
 function die(message) {
-    log(`error: ${message}`);
+	log(`error: ${message}`);
 
-    process.exit(1);
+	process.exit(1);
 }
 
 function log(message) {
-    process.stderr.write(`${message}\n`);
+	process.stderr.write(`${message}\n`);
 }
 
 main();
