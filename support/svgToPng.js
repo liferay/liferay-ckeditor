8acb98c5ee9f30aa68964e8821f8a9033f02b270
Carlos Lancha <carlos.lancha@liferay.com>
2020-06-22 22:45:32 +0200

    feat: format and change icons JSON format

--- support/svgToPng.js
+++ support/svgToPng.js
@@ -1,38 +1,41 @@
-const fs = require('fs')
-const path = require( 'path' )
-const sharp = require("sharp")
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
 
 const configFilePath = process.argv[2];
-const outputDir = process.argv[3]
+const outputDir = process.argv[3];
 
-let rawdata = fs.readFileSync(configFilePath)
-let iconsConfig = JSON.parse(rawdata)
+const rawData = fs.readFileSync(configFilePath);
+const iconsConfig = JSON.parse(rawData);
 
-const sourceIconsPath = path.join(path.dirname(configFilePath), '../', iconsConfig.dir)
+const sourceIconsPath = path.join(
+	path.dirname(configFilePath),
+	'..',
+	iconsConfig.dir
+);
 
-for (const icon of iconsConfig.icons) {
-    sharp(`${sourceIconsPath}/${icon.source}.svg`)
-        .resize(16)
+for (const [source, output] of Object.entries(iconsConfig.icons)) {
+	sharp(`${sourceIconsPath}/${icon.source}.svg`)
+		.resize(16)
 		.png()
 		.toFile(path.resolve(`${outputDir}/${icon.output}.png`))
 		.then(() => {
-			console.log(`${icon.source} SVG icon converted to PNG`)
+			console.log(`${icon.source} SVG icon converted to PNG`);
 		})
-		.catch(err => {
-            console.log(`${icon.source} ${err}`)
-            process.exit(1);
-		})
+		.catch((err) => {
+			console.log(`${icon.source} ${err}`);
+			process.exit(1);
+		});
 
-    sharp(`${sourceIconsPath}/${icon.source}.svg`)
-        .resize(32)
-        .tint('rgb(39,40,51)')
+	sharp(`${sourceIconsPath}/${icon.source}.svg`)
+		.resize(32)
 		.png()
 		.toFile(path.resolve(`${outputDir}/hidpi/${icon.output}.png`))
 		.then(() => {
-			console.log(`${icon.source} SVG icon converted to HIDPI PNG`)
+			console.log(`${icon.source} SVG icon converted to HIDPI PNG`);
 		})
-		.catch(err => {
-            console.log(`${icon.source} ${err}`)
-            process.exit(1);
-        })
+		.catch((err) => {
+			console.log(`${icon.source} ${err}`);
+			process.exit(1);
+		});
 }
