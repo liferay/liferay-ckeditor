979d2bfcf6ac1eb49bc056ccee09d26a21c46976
Julien Castelain <julien.castelain@liferay.com>
2020-08-17 12:07:56 +0200

    chore: update ckeditor build artifacts
    This commit autogenerated with "./ck.sh".

--- ckeditor/plugins/widget/lang/es.js
+++
@@ -1,5 +0,0 @@
-﻿/*
- Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
- For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
-*/
-CKEDITOR.plugins.setLang("widget","es",{move:"Dar clic y arrastrar para mover",label:"reproductor %1"});
--- ckeditor/plugins/widget/lang/it.js
+++
@@ -1,5 +0,0 @@
-﻿/*
- Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
- For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
-*/
-CKEDITOR.plugins.setLang("widget","it",{move:"Fare clic e trascinare per spostare",label:"Widget %1"});
