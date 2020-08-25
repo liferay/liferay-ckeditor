ce3701da59718ffbb64301c84e678a55762b4e7a
Carlos Lancha <carlos.lancha@liferay.com>
2020-08-25 08:48:48 +0200

    chore: sf

--- support/copyPluginDependencies.js
+++ support/copyPluginDependencies.js
@@ -3,45 +3,29 @@ const path = require('path');
 
 const pluginDir = process.argv[2];
 
-const configFilePath = path.join(
-	pluginDir,
-	'deps.json'
-);
+const configFilePath = path.join(pluginDir, 'deps.json');
 
-const depsDir = path.join(
-	pluginDir,
-	'deps'
-);
+const depsDir = path.join(pluginDir, 'deps');
 
 const rawData = fs.readFileSync(configFilePath);
 const depsConfig = JSON.parse(rawData);
 
 if (!fs.existsSync(depsDir)) {
-    fs.mkdirSync(depsDir);
+	fs.mkdirSync(depsDir);
 }
 
 for (const [dep, files] of Object.entries(depsConfig)) {
-    const outputDir = path.join(
-        depsDir,
-        dep
-    );
+	const outputDir = path.join(depsDir, dep);
 
-    if (!fs.existsSync(outputDir)) {
-        fs.mkdirSync(outputDir);
-    }
+	if (!fs.existsSync(outputDir)) {
+		fs.mkdirSync(outputDir);
+	}
 
-    files.forEach(file => {
-        const filePath = path.join(
-            '../node_modules',
-            dep,
-            file
-        );
+	files.forEach((file) => {
+		const filePath = path.join('../node_modules', dep, file);
 
-        const fileOutput = path.join(
-            outputDir,
-            path.basename(file)
-        );
+		const fileOutput = path.join(outputDir, path.basename(file));
 
-        fs.copyFileSync(filePath, fileOutput)
-    });
+		fs.copyFileSync(filePath, fileOutput);
+	});
 }
