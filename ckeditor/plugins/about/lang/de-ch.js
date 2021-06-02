f2ee22cb6990842b7e56abbc2df8a8e1e2fff2d8
Jonathan Mak <jonathan.mak@liferay.com>
2021-06-02 10:36:12 -0700

    chore: update ckeditor build artifacts
    This commit autogenerated with "./ck.sh".

--- ckeditor/plugins/about/lang/de-ch.js
+++ ckeditor/plugins/about/lang/de-ch.js
@@ -1,1 +1,1 @@
-﻿CKEDITOR.plugins.setLang("about","de-ch",{copy:"Copyright \x26copy; $1. Alle Rechte vorbehalten.",dlgTitle:"Über CKEditor 4",moreInfo:"Für Informationen über unsere Lizenzbestimmungen besuchen sie bitte unsere Webseite:"});
+﻿CKEDITOR.plugins.setLang("about","de-ch",{copy:"Copyright \x26copy; $1. Alle Rechte vorbehalten.",dlgTitle:"Über CKEditor 4",moreInfo:"Für Informationen über unsere Lizenzbestimmungen besuchen Sie bitte unsere Webseite:"});
