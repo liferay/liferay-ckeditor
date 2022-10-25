6c4363e8cf54633d2ef8dcfe994420d1ca0d5e45
Victor Galan <victor.galan@liferay.com>
2022-10-25 15:56:50 +0200

    fix: LPS-165655 Disable tab key by default to avoid the focus to be trapped in the editor

--- plugins/codemirror/lang/en.js
+++ plugins/codemirror/lang/en.js
@@ -1,4 +1,6 @@
 CKEDITOR.plugins.setLang('codemirror', 'en', {
+	disableTabKeyUsing: 'Disable tab key using:',
+	enableTabKeyUsing: 'Enable tab key using:',
 	preview: 'Preview',
 	source: 'Source',
 });
